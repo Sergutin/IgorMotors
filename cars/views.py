9700d1d683c90faa17bfd2a670d444a49497eb05
Sergutin <igor@sergutin.com>
2023-08-07 15:34:08 +0000

    Added cars views and templates

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Car
 
 # Create your views here.
+
+def all_cars(request):
+    """ A view to show all cars """
+
+    cars = Car.objects.all()
+
+    context = {
+        'cars': cars,
+    }
+
+    return render(request, 'cars/cars.html', context)
