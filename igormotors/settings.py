b9b226f4f418d3c96a4ca3b8d241ce214c54c651
Sergutin <igor@sergutin.com>
2023-08-07 11:49:37 +0000

    Completed homepahe header and css

--- igormotors/settings.py
+++ igormotors/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(3h#-a-o!z(5nc-gx(9l&--jge=y$+%6(c+@l#m^$oo@=bk8)y'
+SECRET_KEY = 'to_be_added'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,6 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
