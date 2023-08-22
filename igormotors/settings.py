b3358fde4fae4cdec8a3bb106bce1a567c2b29ae
Sergutin <igor@sergutin.com>
2023-08-22 17:01:55 +0000

    Fix css issue to move page titles down

--- igormotors/settings.py
+++ igormotors/settings.py
@@ -25,13 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'to_be_added'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-sergutin-igormotors-vkcathx15yz.ws-eu104.gitpod.io',
+ALLOWED_HOSTS = ['8000-sergutin-igormotors-to7t6io0irv.ws-eu104.gitpod.io',
                  'igormotors.heroku.com', 'localhost',
                  '.herokuapp.com',
                  'igormotors-fa74a181bdfa.herokuapp.com']
