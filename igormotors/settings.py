c9686ad96ddf3161b36b2db8aa20a5e1cea88891
Sergutin <igor@sergutin.com>
2023-08-24 18:29:24 +0000

    Setup mailchimp

--- igormotors/settings.py
+++ igormotors/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'to_be_added'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sergutin-igormotors-eogcvgfwfbv.ws-eu104.gitpod.io',
                  'igormotors.heroku.com', 'localhost',
