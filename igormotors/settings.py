8722f41c1316397776945d32501603678bdf80c8
Sergutin <igor@sergutin.com>
2023-08-26 09:10:06 +0000

    Add getcookie function

--- igormotors/settings.py
+++ igormotors/settings.py
@@ -25,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+# SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'to_be_added'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-sergutin-igormotors-0adfcel6su7.ws-eu104.gitpod.io',
+ALLOWED_HOSTS = ['8000-sergutin-igormotors-sslthh4j6by.ws-eu104.gitpod.io',
                  'igormotors.heroku.com', 'localhost',
                  '.herokuapp.com',
                  'igormotors-fa74a181bdfa.herokuapp.com']
